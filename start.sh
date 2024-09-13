b963b61e5f1cd828bdb95c5a89decdbc6d090725
Jacob Leazott <jacob.leazott@gmail.com>
2024-09-13 12:24:01 -0600

    Fixed start command and added .desktop file

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@
 # LOG_PATH  = $PROJ_PATH/logs
 # @reboot $PROJ_PATH/start.sh > $LOG_PATH/Album-Display.log
 
+#sleep 10
 DISPLAY=:0 xrandr --output HDMI-1 --rotate right
-cd $PROJ_PATH
-python album_display.py
+source ~/.venv/bin/activate
+cd /home/pi/projcets/Album_Artwork_Display
+DISPLAY=:0 python Display.py
